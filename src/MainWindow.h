12b2ad1edec79ad52558c285c2ea74ac1f27d1d7
Johannes Marbach <jm@elroy.localdomain>
2011-11-26 21:33:33 +0100

    Added license notes and included license file in distribution tarbal

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -1,25 +1,8 @@
+//////////////////////////////////////////////////////////////////////////////
+// MainWindow.h
 //
-//
-//      MainWindow.h
-//      
-//      Copyright 2011 Johannes Marbach <JohannesMarbach@googlemail.com>
-//      
-//      This program is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published by
-//      the Free Software Foundation; either version 2 of the License, or
-//      (at your option) any later version.
-//      
-//      This program is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU General Public License for more details.
-//      
-//      You should have received a copy of the GNU General Public License
-//      along with this program; if not, write to the Free Software
-//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//      MA 02110-1301, USA.
-//      
-//      
+// Copyright 2011 Johannes Marbach. All rights reserved.
+// See the LICENSE file for details.
 
 
 #ifndef H_MAIN_WINDOW
